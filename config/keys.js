1afcc88a19227b72103fce3e037c9c85f8db9085
Devanshu Dhandhal <divanshudhandhal@gmail.com>
2021-06-26 19:12:05 +0530

    Heroku env variables

--- config/keys.js
+++ config/keys.js
@@ -1,16 +1,6 @@
-// module.exports = {
-//   googleClientID: process.env.GOOGLE_CLIENT_ID,
-//   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//   mongoURI: process.env.MONGO_URI,
-//   cookieKey: process.env.COOKIE_KEY,
-// };
-
 module.exports = {
-  googleClientID:
-    "28311856461-efl9dsd4gotv3pm61l8e41jndc9nqbv4.apps.googleusercontent.com",
-  googleClientSecret: "Ij4M8pykw3MO12F7OoVPyKlO",
-  mongoURI:
-    "mongodb+srv://dbUser:Pass*123@home-services-app.fi1ya.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  cookieKey:
-    "vbsdukfbvkusdbuikvbuihfahfghfgafghbaiofghvuieashfgvyureofghuiorewhfguioheruiohfgerohuiogherui",
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
 };
